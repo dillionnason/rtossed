4a73c1de2ddab5cb65659271ccd38c24c9b11d08
Dillion James Nason <dillion.nason@maine.edu>
2023-02-10 11:07:13 -0500

    inline yield

--- Core/Inc/process.h
+++ Core/Inc/process.h
@@ -1,6 +1,15 @@
 #ifndef __PROCESS_H
 #define __PROCESS_H
 
+#include <main.h>
+
 extern int kready;
+
+inline void yield(void)
+{
+	SCB->ICSR |= SCB_ICSR_PENDSVSET_Msk;
+	__DSB();			// Data Synchronization Barrier
+	__ISB();			// Instruction Synchronization Barrier
+}
 
 #endif // __PROCESS_H
