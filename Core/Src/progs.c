81e4b52b8f263f5a6bca04699af722f0a17aa4da
Dillion James Nason <dillion.nason@maine.edu>
2023-03-08 18:50:50 -0500

    Switched to microsleep in processes

--- Core/Src/progs.c
+++ Core/Src/progs.c
@@ -2,6 +2,7 @@
 #include "gpio.h"
 #include "stm32h7xx_hal.h"
 #include "stm32h7xx_hal_gpio.h"
+#include "user_syscalls.h"
 
 /**
 	* Blink the green LED (LD1) at 2Hz
@@ -10,7 +11,7 @@ int process1(void)
 {
 	while (1) {
 		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
-		HAL_Delay(500);
+		microsleep(500000);
 	}
 	return 0;
 }
@@ -22,6 +23,6 @@ void process2(void)
 {
 	while (1) {
 		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-		HAL_Delay(1000);
+		microsleep(1000000);
 	}
 }
