c87ef8daa86961beac5db9361b6bd26dc7453c33
Dillion Nason <nasond432@gmail.com>
2023-04-04 11:48:29 -0400

    Added printfs, changed baud rate

--- Core/Src/progs.c
+++ Core/Src/progs.c
@@ -3,13 +3,18 @@
 #include "stm32h7xx_hal.h"
 #include "stm32h7xx_hal_gpio.h"
 #include "user_syscalls.h"
+#include "process.h"
+#include <stdint.h>
+#include <stdio.h>
 
 /*
  * Blink the green LED (LD1) at 2Hz
  */
 int process1(void)
 {
+	static uint8_t count = 0;
 	while (1) {
+		printf("Current: %d | The current value of i is: %u\r\n", current->pid, count++);
 		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
 		microsleep(500000);
 	}
@@ -21,7 +26,9 @@ int process1(void)
  */
 void process2(void)
 {
+	static uint8_t count = 0;
 	while (1) {
+		printf("Current: %d | The current value of i is: %u\r\n", current->pid, count++);
 		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 		microsleep(1000000);
 	}
